837ee3800e2f3f7501670a012f962a039f0c0f05
Stasis, the Shattered <99687421+StainlessStasis@users.noreply.github.com>
2026-07-01 23:54:37 -0400

    Fix neo server crash

--- common/src/main/java/io/github/stainlessstasis/skytrader/entity/SkyTraderGhast.java
+++ common/src/main/java/io/github/stainlessstasis/skytrader/entity/SkyTraderGhast.java
@@ -153,12 +153,12 @@ public class SkyTraderGhast extends HappyGhast implements TraceableEntity, Ownab
         }
 
         if (!this.rideState.isStartOfRide()) {
-            if (!hasPaid(player)) {
-                player.sendOverlayMessage(Component.translatable(ModConstants.MOD_ID + ".ride_already_started").withColor(TextColor.RED));
-                return InteractionResult.FAIL;
-            }
             if (this.rideState.isEndOfRide()) {
                 player.sendOverlayMessage(Component.translatable(ModConstants.MOD_ID + ".ride_already_ended").withColor(TextColor.RED));
+                return InteractionResult.FAIL;
+            }
+            if (!hasPaid(player)) {
+                player.sendOverlayMessage(Component.translatable(ModConstants.MOD_ID + ".ride_already_started").withColor(TextColor.RED));
                 return InteractionResult.FAIL;
             }
         }
