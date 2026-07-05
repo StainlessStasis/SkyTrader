ec4f0cb62da179cba643ae8632d2ceb2e725db58
Stasis, the Shattered <99687421+StainlessStasis@users.noreply.github.com>
2026-07-04 23:10:04 -0400

    Fix Skyflare summoning traders even when not used on ground | Add 2 new advancements

--- common/src/main/java/io/github/stainlessstasis/skytrader/advancement/ModAdvancements.java
+++ common/src/main/java/io/github/stainlessstasis/skytrader/advancement/ModAdvancements.java
@@ -8,8 +8,13 @@ import net.minecraft.server.level.ServerPlayer;
 public class ModAdvancements {
     public static final Identifier ROOT = ModConstants.id("root");
     public static final Identifier WELCOME_ABOARD = ModConstants.id("welcome_aboard");
+
     public static final Identifier SNACK_RUN = ModConstants.id("snack_run");
     public static final Identifier FIRST_CLASS_CUSTOMER = ModConstants.id("first_class_customer");
+
+    public static final Identifier YOU_CALLED = ModConstants.id("you_called");
+    public static final Identifier THERES_NO_SKY_HERE = ModConstants.id("theres_no_sky_here");
+
     public static final Identifier FREE_BIRD = ModConstants.id("free_bird");
     public static final Identifier NO_FLY_LIST = ModConstants.id("no_fly_list");
 
