0e429c8943fc8153cf35008bdf480c38d177a80e
Stasis, the Shattered <99687421+StainlessStasis@users.noreply.github.com>
2026-07-05 03:41:57 -0400

    A Terrible Day for Rain advancement

--- common/src/main/java/io/github/stainlessstasis/skytrader/entity/SkyTraderGhast.java
+++ common/src/main/java/io/github/stainlessstasis/skytrader/entity/SkyTraderGhast.java
@@ -818,6 +818,10 @@ public class SkyTraderGhast extends HappyGhast implements TraceableEntity, Ownab
             return;
         }
 
+        if (target instanceof ServerPlayer player) {
+            ModAdvancements.grant(player, ModAdvancements.TERRIBLE_DAY_FOR_RAIN);
+        }
+
         if (isLeashed()) {
             dropLeash();
         }
